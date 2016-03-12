bf8d7f57184ce4bb2ca04096c1f1ba057e091f9d
王正一 <fsymbio@icloud.com>
2016-03-12 20:45:38 +0800

    添加pod Spec成功

---
+++ MyTestPods/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  MyTestPods
+//
+//  Created by qufenqi on 16/3/12.
+//  Copyright © 2016年 王正一. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
